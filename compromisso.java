ddc688d882630ec86d47367b65f6f3e96f561c1d
xaisterr <139409499+xaisterr@users.noreply.github.com>
2025-05-13 14:42:43 -0300

    Update compromisso.java

--- compromisso.java
+++ compromisso.java
@@ -1,1 +1,25 @@
+import  java.util.Scanner;
+public class compromisso {
+    String nome;
+    String datahorario;
+    String descricao;
+    String local;
+    int prioridade;
+    String listaenvolvidos;
 
+    public void LerParametros(){
+        System.out.println("Digite nome, data e hora, descrição do evento, local, nivel de prioridade e a lista dos envolvidos");
+        Scanner sc = new Scanner(System.in);
+        this.nome = sc.nextLine();
+        this.datahorario = sc.nextLine();
+        this.descricao = sc.nextLine();
+        this.local = sc.nextLine();
+        this.prioridade = sc.nextInt();
+        sc.nextLine();
+        this.listaenvolvidos = sc.nextLine();
+    }
+    public void MostraParametros(){
+        System.out.println("Compromisso: " + this.nome + " \ndata/horario: " + this.datahorario + " \nDescrição: " + this.descricao + " \nLocalização: " + this.local + " \nnivel de prioridade: " + this.prioridade + " \nLista de envolvidos: " + this.listaenvolvidos);
+        System.out.println();
+    }
+}
